def76bf0b5bb4dbfb49eb97842aaa8ffe9ca0221
Jose Moreno <jose.moreno@microsoft.com>
2022-04-26 11:07:40 +0200

    BIRD VMSS

---
+++ routeserver-vmss-selfcontained-healthcheck.py
@@ -0,0 +1,40 @@
+import os
+import socket, struct
+import sys
+import time
+import warnings
+import requests
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+# Gets the web port out of an environment variable, or defaults to 8080
+def get_web_port():
+    web_port=os.environ.get('PORT')
+    if web_port==None or not web_port.isnumeric():
+        print("Using default port 8080")
+        web_port=8080
+    else:
+        print("Port supplied as environment variable:", web_port)
+    return web_port
+
+# Flask route for healthchecks
+@app.route("/api/healthcheck", methods=['GET'])
+def healthcheck():
+    if request.method == 'GET':
+        try:
+          msg = {
+            'health': 'OK'
+          }          
+          return jsonify(msg)
+        except Exception as e:
+          return jsonify(str(e))
+
+# Ignore warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+# Set web port
+web_port=get_web_port()
+
+app.run(host='0.0.0.0', port=web_port, debug=True, use_reloader=False)
