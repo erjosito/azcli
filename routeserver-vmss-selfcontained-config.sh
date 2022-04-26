36db9eed275f33ebc38114344adc7e93bcdac0b7
Jose Moreno <jose.moreno@microsoft.com>
2022-04-26 11:25:06 +0200

    changes

--- routeserver-vmss-selfcontained-config.sh
+++ routeserver-vmss-selfcontained-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 log_file='/root/routeserver.log'
+date >>$log_file
 metadata=$(curl -s -H Metadata:true --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2021-02-01" | jq)
-echo $metadata >$log_file
+echo "$metadata" >>$log_file
